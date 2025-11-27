3d6ebc6bb7f7352ea9f99333318e31b0af5a2f7c
agld-c06 <aluiscasas06@gmail.com>
2025-11-27 23:44:11 +0800

    edited to match my .asm

--- AndaKCasaLmp2.c
+++ AndaKCasaLmp2.c
@@ -2,27 +2,28 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-extern void imgCvtGrayDoubleToInt(double** doubleArray, uint8_t** uInt8Array, int row, int col);
+extern void imgCvtGrayDoubleToInt(int row, int col, double** doubleArray, uint8_t** uInt8Array);
 
 int main(void) {
     int row, col;
+    int i, j;
     scanf("%d %d", &row, &col);
 
     double **doubleArray = malloc(row * sizeof *doubleArray);
     uint8_t **uInt8Array = malloc(row * sizeof *uInt8Array);
 
-    for (int i = 0; i < row; i++){
+    for (i = 0; i < row; i++){
         doubleArray[i] = malloc(col * sizeof *doubleArray[i]);
         uInt8Array[i] = malloc(col * sizeof *uInt8Array[i]);
 
-        for (int j = 0; j < col; j++)
+        for (j = 0; j < col; j++)
             scanf("%lf", &doubleArray[i][j]);
     }
 
-    imgCvtGrayDoubleToInt(doubleArray, uInt8Array, row, col);
+    imgCvtGrayDoubleToInt(row, col, doubleArray, uInt8Array);
 
-    for (int i = 0; i < row; i++) {
-        for (int j = 0; j < col; j++) printf("%d ", uInt8Array[i][j]);
+    for (i = 0; i < row; i++) {
+        for (j = 0; j < col; j++) printf("%d ", uInt8Array[i][j]);
         printf("\n");
     }
 
--- imgCvtGrayDoubleToInt.asm
+++ imgCvtGrayDoubleToInt.asm
@@ -1,41 +1,41 @@
-; rcx = pointer to doubleArray
-; rdx = pointer to uInt8Array
-; r8  = row
-; r9  = col
-
-global imgCvtGrayDoubleToInt
-section .text
 bits 64
-Default rel
-
-imgCvtGrayDoubleToInt:
-    test r8, r8
-    mov rbx, r9
-    jz .done
-
-.loop:
-    mov rdi, [rcx]
-    mov rsi, [rdx]
-    mov r9, rbx
-    
-    .inner_loop:
-        movsd xmm0, [rdi]        
-        mulsd xmm0, [rel scale]  
-        cvttsd2si rax, xmm0      
-        mov [rsi], al
-  
-        add rdi, 8               
-        add rsi, 1               
-        dec r9
-        jnz .inner_loop
-        
-    add rcx, 8
-    add rdx, 8
-    dec r8
-    jnz .loop
-
-.done:
-    ret
+global imgCvtGrayDoubleToInt
 
 section .data
 scale: dq 255.0
+
+section .text
+imgCvtGrayDoubleToInt:
+    ; rcx = row
+    ; rdx = col
+    ; r8 = doubleArray (pointer to array of pointers)
+    ; r9 = uInt8Array (pointer to array of pointers)
+    
+    test rcx, rcx
+    jz done
+
+rowLoop:
+    mov rsi, [r8]          ; load doubleArray[i] pointer
+    mov rdi, [r9]          ; load uInt8Array[i] pointer
+    mov rbx, rdx           ; column counter
+    
+colLoop:
+    movsd xmm0, [rsi]      ; load double value
+    mulsd xmm0, [scale]    ; multiply by 255.0
+    cvttsd2si rax, xmm0    ; convert to integer
+    mov [rdi], al          ; store uint8 value
+    
+    add rsi, 8             ; next double (8 bytes)
+    add rdi, 1             ; next uint8 (1 byte)
+    
+    dec rbx
+    jnz colLoop
+    
+    add r8, 8              ; next input row pointer
+    add r9, 8              ; next output row pointer
+    
+    dec rcx
+    jnz rowLoop
+
+done:
+    ret
