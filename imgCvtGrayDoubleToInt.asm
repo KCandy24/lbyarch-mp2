2d70e6f88490ad23cbb649c77b8592ecdf725521
agld-c06 <aluiscasas06@gmail.com>
2025-11-28 08:25:14 +0800

    Update imgCvtGrayDoubleToInt.asm

--- imgCvtGrayDoubleToInt.asm
+++ imgCvtGrayDoubleToInt.asm
@@ -16,24 +16,24 @@ imgCvtGrayDoubleToInt:
     jz done
 
 rowLoop:
-    mov rsi, [r8]          ; load doubleArray[i] pointer
-    mov rdi, [r9]          ; load uInt8Array[i] pointer
-    mov rbx, rdx           ; column counter
+    mov rsi, [r8]       ; load doubleArray[i] pointer
+    mov rdi, [r9]       ; load uInt8Array[i] pointer
+    mov rbx, rdx        ; column counter
     
 colLoop:
-    movsd xmm0, [rsi]      ; load double value
-    mulsd xmm0, [scale]    ; multiply by 255.0
-    cvtsd2si rax, xmm0    ; convert to integer
-    mov [rdi], al          ; store uint8 value
+    movsd xmm0, [rsi]       ; load double value
+    mulsd xmm0, [scale]     ; multiply by 255.0
+    cvtsd2si rax, xmm0      ; convert to integer
+    mov [rdi], al           ; store uint8 value
     
-    add rsi, 8             ; next double (8 bytes)
-    add rdi, 1             ; next uint8 (1 byte)
+    add rsi, 8      ; next double (8 bytes)
+    add rdi, 1      ; next uint8 (1 byte)
     
     dec rbx
     jnz colLoop
     
-    add r8, 8              ; next input row pointer
-    add r9, 8              ; next output row pointer
+    add r8, 8       ; next input row pointer
+    add r9, 8       ; next output row pointer
     
     dec rcx
     jnz rowLoop
